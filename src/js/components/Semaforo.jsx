3913a1b4e7eedbc49b0eecc7aaba3e0c70e53083
cristina1810 <cristina.pavlusiv@gmail.com>
2025-07-10 13:30:26 +0200

    terminado

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -1,48 +1,78 @@
 import React, { useState } from "react";
-import "../../styles/index.css"; 
-import Boton from "./Boton"
+import "../../styles/index.css";
+import Boton from "./Boton";
 
 const Semaforo = () => {
-    const [color, setColor] = useState('');
-    const [purpleExist, setPurpleExist] = useState(false);
+  const [color, setColor] = useState("");
+  const [purpleExist, setPurpleExist] = useState(false);
 
-    function turnRed () {
-           setColor('roja')
+  function turnRed() {
+    setColor("roja");
+  }
+
+  function turnGreen() {
+    setColor("verde");
+  }
+
+  function turnYellow() {
+    setColor("amarilla");
+  }
+  function turnPurple() {
+    setColor("purp");
+  }
+
+  function alternar() {
+    let funcionesActuales = [turnRed, turnGreen, turnYellow];
+    if (purpleExist) {
+      funcionesActuales.push(turnPurple);
     }
 
-    function turnGreen () {
-           setColor('verde')
-    }
-    
-    function turnYellow () {
-          setColor('amarilla')
-    }
+    const funcionAleatoria =
+      funcionesActuales[Math.floor(Math.random() * funcionesActuales.length)];
+    funcionAleatoria();
+  }
 
-    const funciones = [turnRed,turnGreen,turnYellow]
+  const btnPurp = () => {
+    setPurpleExist((prev) => (purpleExist === false ? true : false));
+  };
 
-    function alternar() {
-    funciones[Math.floor(Math.random()*funciones.length)]();
-    }
-    
-    const btnPurp = () => {
-        setPurpleExist(prev => purpleExist ===false ? true : false  );
-    }
-
-    return (
-        <div>
-        <div className="fondoSem d-flex flex-column align-items-center p-3 gap-3 ">
-            <div className={`luz bg-danger ${color === 'roja' ? 'roja' : ''} `} onClick={() => turnRed()}></div>
-            <div className={`luz bg-warning ${color === 'amarilla' ? 'amarilla' : ''}`} onClick={() => {turnYellow()}}></div>
-            <div className={`luz bg-success ${color === 'verde' ? 'verde' : ''}`} onClick={() => {turnGreen()}}></div>
-            {/* <div className="luz bg-purp" onClick={() => {}}></div> */}
-        </div>
-        <div className="m-3">
-            <Boton titulo="Alternar" onClick={alternar}/>
-            <Boton titulo="Mostrar" onClick={btnPurp}/>
-
-        </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="fondoSem d-flex flex-column align-items-center p-3 gap-3 ">
+        <div
+          className={`luz bg-danger ${color === "roja" ? "roja" : ""} `}
+          onClick={() => turnRed()}
+        ></div>
+        <div
+          className={`luz bg-warning ${color === "amarilla" ? "amarilla" : ""}`}
+          onClick={() => {
+            turnYellow();
+          }}
+        ></div>
+        <div
+          className={`luz bg-success ${color === "verde" ? "verde" : ""}`}
+          onClick={() => {
+            turnGreen();
+          }}
+        ></div>
+        {purpleExist && (
+          <div
+            className={`luz bg-purp ${color === "purp" ? "purp" : ""}`}
+            onClick={() => {
+              turnPurple();
+            }}
+          ></div>
+        )}
+      </div>
+      <div className="m-3">
+        <Boton titulo="Alternar" onClick={alternar} />
+        <Boton
+          titulo={`${purpleExist ? "Ocultar" : "Mostrar"}`}
+          onClick={btnPurp}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Semaforo;
